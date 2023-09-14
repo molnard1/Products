ae12f9f3a65a51441921c790e6d2954557a21234
molnard1 <101341428+molnard1@users.noreply.github.com>
2023-09-14 11:13:18 +0200

    feat: Implement routes for getting, adding, modifying, and deleting products.

--- ProductAPI/Dtos.cs
+++ ProductAPI/Dtos.cs
@@ -2,6 +2,10 @@
 {
     public class Dtos
     {
-        public record ProductDto(Guid Id, string ProductNeve, int ProductPrice, DateTimeOffset CreatedTime, DateTimeOffset ModifiedTime);
+        public record ProductDto(Guid Id, string ProductName, int ProductPrice, DateTimeOffset CreatedTime, DateTimeOffset ModifiedTime);
+
+        public record CreateObjectDto(string ProductName, int ProductPrice);
+
+        public record UpdateProductDto(string ProductName, int ProductPrice);
     }
 }
